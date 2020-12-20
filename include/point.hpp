f13c9ce2de0e56d08da90275fab560743c2dd08a
atharva-18 <atharvapusalkar18@gmail.com>
2020-12-20 16:18:18 +0530

    Add constructor

--- include/point.hpp
+++ include/point.hpp
@@ -8,14 +8,17 @@ class Point
 {
 public:
 
-  // Contructor
-//   Point();
-
-  // Destructor
-//   ~Point();
+  // Constructor
+  Point();
+  Point(double _x,
+        double _y,
+        const double _parentX=0.0,
+        const double _parentY=0.0,
+        const std::vector<Point> _path={});
 
   double x;
   double y;
-  std::shared_ptr<Point> parent;
+  double parentX;
+  double parentY;
   std::vector<Point> path;
 };
--- src/point.cpp
+++ src/point.cpp
@@ -1,9 +1,18 @@
 #include "point.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
-Point::Point(double _x, double _y)
-: x(_x), y(_y)
-{}
+Point::Point(double _x,
+             double _y,
+             const double _parentX /*=0.0*/,
+             const double _parentY /*=0.0*/,
+             const std::vector<Point> _path /*={}*/)
+{
+  x = _x;
+  y = _y;
+  parentX = _parentX;
+  parentY = _parentY;
+  path = _path;
+}
 
 Point::Point()
 : x(0.0), y(0.0)
