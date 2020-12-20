6aa0c010eca9532a5f5e6e591ad50a402c9c05e4
atharva-18 <atharvapusalkar18@gmail.com>
2020-12-20 21:21:36 +0530

    Add external config

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,53 @@
 #include "matplotlibcpp.h"
 
 #include <vector>
+#include <fstream>
+#include <cmath>
 
 namespace plt = matplotlibcpp;
 using json = nlohmann::json;
 
-int main() {
-  Point start(0.0, 0.0);
+////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+  std::ifstream iConfig("../Params/config.json");
+  json jsonConfig;
+  iConfig >> jsonConfig;
+
+  auto startPoint = jsonConfig["start"];
+  auto endPoint = jsonConfig["end"];
+  auto obs = jsonConfig["obstacles"];
+  auto maxStep = jsonConfig["max_step"];
+  auto iterations = jsonConfig["iterations"];
+  auto sampleRangeMin = jsonConfig["sample_range_min"];
+  auto sampleRangeMax = jsonConfig["sample_range_max"];
+  auto precision = jsonConfig["precision"];
+
+  Point start(startPoint[0], startPoint[1]);
   start.parentX = start.x;
   start.parentY = start.y;
   start.path.push_back(start);
-  Point end(6.0, 10.0);
+  Point end(endPoint[0], endPoint[1]);
     
-  std::vector<Obstacle> obstacles = {Obstacle(5.0, 5.0, 1.0),
-                                     Obstacle(3.0, 6.0, 1.0),
-                                     Obstacle(3, 8, 1.0),
-                                     Obstacle(3, 10, 1.0),
-                                     Obstacle(7, 5, 1.0),
-                                     Obstacle(9, 5, 1.0),
-                                     Obstacle(8, 10, 1.0)};
+  std::vector<Obstacle> obstacles;
 
-  std::vector<double> ox;
-  std::vector<double> oy;
-
-  for(auto &obstacle: obstacles) {
-    ox.push_back(obstacle.x);
-    oy.push_back(obstacle.y);
+  for(auto _obs:obs) {
+    obstacles.push_back(Obstacle(_obs[0], _obs[1], _obs[2]));
   }
 
-  plt::scatter(ox, oy, 100.0);
+  for(auto &obstacle: obstacles) {
+    std::vector<double> ox;
+    std::vector<double> oy;
+    for (int deg = -1; deg < 360; deg += 5) {
+      double rad = deg * M_PI/180.0;
+      ox.push_back(obstacle.x + obstacle.a * std::cos(rad));
+      oy.push_back(obstacle.y + obstacle.a * std::sin(rad));
+    }
 
-  RRT rrt(start, end, obstacles);
+    plt::plot(ox, oy, "#0a192f");
+  }
+
+  RRT rrt(start, end, obstacles, maxStep, iterations, sampleRangeMin, sampleRangeMax, precision);
 
   bool ok = rrt.run();
 
@@ -51,9 +68,6 @@ int main() {
   for (auto &point:points) {
     std::vector<double> x = {point.x, point.parentX};
     std::vector<double> y = {point.y, point.parentY};
-
-    // std::cout << "(" << x[0] << "," << y[0] << ")" << std::endl;
-    // std::cout << "(" << x[1] << "," << y[1] << ")" << std::endl;
 
     plt::plot(x, y, "g");
     plt::pause(0.01);
